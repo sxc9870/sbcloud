c5338356ad90e91757b97ee75de696a546786129
shixuechen <35631371@qq.com>
2019-04-16 09:13:28 +0800

    fx

--- parent/core/src/main/java/com/sbcloud/StartApplication.java
+++ parent/core/src/main/java/com/sbcloud/StartApplication.java
@@ -1,15 +1,30 @@
 package com.sbcloud;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RefreshScope // 自动刷新配置变更
+@EnableDiscoveryClient
+@RestController
 public class StartApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StartApplication.class, args);
 	}
 
+	@Value("${abc}")
+	private String aaa;
+
+	@GetMapping("/")
+	public String a() {
+		return aaa;
+	}
 }
