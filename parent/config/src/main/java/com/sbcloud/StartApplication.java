2c4e4537a5e81b2131364ad4d14350bdfa0e3524
shixuechen <35631371@qq.com>
2019-04-17 16:15:27 +0800

    fxx

--- parent/config/src/main/java/com/sbcloud/StartApplication.java
+++ parent/config/src/main/java/com/sbcloud/StartApplication.java
@@ -2,14 +2,12 @@ package com.sbcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.client.ConfigServicePropertySourceLocator;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
+//@EnableEurekaClient
 public class StartApplication {
 
 	public static void main(String[] args) {
